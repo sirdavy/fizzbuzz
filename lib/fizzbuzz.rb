d9b358308de973e15c12e9a86fbbe65aea4af262
David Scott <sirdavidscott@hotmail.com>
2022-11-29 12:27:57 +0000

    final commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
-  'fizz'
+  
+    if number % 3 == 0 && number % 5 == 0
+      return "fizzbuzz"
+    elsif number % 3 == 0
+      return "fizz"
+    elsif number % 5 == 0
+      return "buzz"
+    else
+      return number
+    end
 end
